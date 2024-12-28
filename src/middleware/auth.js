3a2bceabf91d057760e9b3427b2e246ea2400589
Khushi <76126723+khushizope@users.noreply.github.com>
2024-12-28 21:14:35 +0530

    Add files via upload

---
+++ src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const User = require('../modals/user')
+const auth =  async(req,res,next)=>{
+    try {
+        const token = req.header('Authorization').replace('Bearer ','')
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await User.findOne({_id:decoded._id,'tokens.token':token})  //find weather that token exists in that array of tokens 
+        if (!user) {
+            throw new Error()
+        }
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({error:"Please authenticate. "})
+    }
+}
+module.exports = auth
